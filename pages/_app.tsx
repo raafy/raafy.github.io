bc5c41bf3657d8b59dc0c9386288b281fd697d31
Raafy <RaafyShiham@gmail.com>
2022-12-18 21:18:33 +0800

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import localFont from "@next/font/local";
+import type { AppProps } from "next/app";
+
+const googleSans = localFont({
+  src: [
+    {
+      path: "../assets/fonts/GoogleSans/GoogleSans-Bold.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/GoogleSans/GoogleSans-BoldItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/GoogleSans/GoogleSans-Italic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/GoogleSans/GoogleSans-Medium.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/GoogleSans/GoogleSans-MediumItalic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/GoogleSans/GoogleSans-Regular.ttf",
+      weight: "100",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={googleSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Styles from "../styles/Home.module.css";
 import Head from "next/head";
+import * as Icon from "react-feather";
 
 export default function index() {
   return (
@@ -9,11 +10,24 @@ export default function index() {
         <title>Home</title>
       </Head>
       <div className={Styles.container}>
-        <header className={Styles.header}>
-          <div></div>
-        </header>
+        <div className={Styles.wrapper}>
+          <header className={Styles.header}>
+            <div>
+              <Icon.Menu color="white" />
+              <Icon.X color="white" />
+            </div>
+            <div>
+              <h1 className={Styles.heading}>Raafy Shiham</h1>
+            </div>
+          </header>
+          <div>
+            <h2 className={Styles.heroText}>
+              Hello! My Name is Ahmed Raafy Shiham.
+            </h2>
+          </div>
 
-        <footer></footer>
+          <footer></footer>
+        </div>
       </div>
     </>
   );
